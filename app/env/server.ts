b0be5bad8a391c5d719fcad1d451180c3fbb203f
Peter van Meijgaard <petervanmeijgaard@gmail.com>
2024-05-19 16:48:18 +0200

    Fix env for Netlify

--- app/env/server.ts
+++ app/env/server.ts
@@ -1,11 +1,23 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
+import "@netlify/edge-functions";
+
+const getEnv = (key: string) => {
+	if (typeof Netlify !== "undefined") {
+		return Netlify.env.get(key);
+	}
+
+	return process.env[key];
+};
 
 export const env = createEnv({
 	server: {
 		TURSO_CONNECTION_URL: z.string().url(),
 		TURSO_AUTH_TOKEN: z.string().min(1),
 	},
-	runtimeEnv: process.env,
+	runtimeEnv: {
+		TURSO_CONNECTION_URL: getEnv("TURSO_CONNECTION_URL"),
+		TURSO_AUTH_TOKEN: getEnv("TURSO_AUTH_TOKEN"),
+	},
 	emptyStringAsUndefined: true,
 });
